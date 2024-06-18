d0c1f9a7cef0c41aa372a32cb6e4655684c46310
Maximiliano <maxi.zelazny@gmail.com>
2024-06-18 20:52:40 -0300

    api

--- src/components/fotos.vue
+++ src/components/fotos.vue
@@ -1,11 +1,13 @@
 <template>
-
+  <div>
     <h1>Catalogo de fotos</h1>
     <ul>
-        <li v-for="foto in fotos" :key="foto.id">
-         {{foto}}
-        </li>
+      <li v-for="foto in fotos" :key="foto.id">
+        <img :src="foto.urls.small" :alt="foto.alt_description" />
+        <p>{{ foto.alt_description }}</p>
+      </li>
     </ul>
+  </div>
 </template>
 
 <script setup>
@@ -13,11 +15,36 @@ import { ref, onMounted } from 'vue'
 import axios from "axios"
 
 const fotos = ref([])
-const fetchfotos = async () => {
-  const response = await axios.get('')
-  fotos.value = response.data
+const fetchFotos = async () => {
+  try {
+    const response = await axios.get('https://api.unsplash.com/photos', {
+      headers: {
+        Authorization: `Client-ID jFk6hRE071mFVIs0cFSNnYQ7cwG3RjkbomypS3hKi08`
+      }
+    })
+    fotos.value = response.data
+  } catch (error) {
+    console.error('Error fetching photos:', error)
+  }
 }
 
-onMounted(fetchfotos)
+onMounted(fetchFotos)
 
 </script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 10px 0;
+}
+
+img {
+  width: 300px;
+  height: auto;
+  border-radius: 8px;
+}
+</style>
