c3e3d27411f2b633d7d7255567a8d9e5750fb6fd
Maximiliano <maxi.zelazny@gmail.com>
2024-06-18 22:08:22 -0300

    version estable

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -37,7 +37,7 @@ const logout = () => {
     router.push("/login")
 }
 const isAuthenticated = () => {
-    if localStorage.getItem("usuario") != null
+  //  if localStorage.getItem("usuario") != null
 }
 
 </script>
