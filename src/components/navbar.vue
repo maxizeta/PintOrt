a690099894a5f7c481a1798afcd3a42a9f2b8991
Maximiliano <maxi.zelazny@gmail.com>
2024-06-20 19:20:54 -0300

    .

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,45 +1,57 @@
 <template>
+    <div>
       <h1>{{ msg }}</h1>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">app</a>
-            <button class="navbar-toggler" type="button">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarnav">
-                <ul class="navbar-nav">
-                    <li>
-                        <router-link class="nav-link" to="/">menu</router-link>
-                    </li>
-                    <li>
-                        <router-link class="nav-link" to="/registroDeUsuario">registrarse</router-link>
-                    </li>
-                    <li>
-                        <router-link class="nav-link" to="/login">login</router-link>
-                    </li>
-                    <li>
-                        <router-link class="nav-link" to="/fotos" v-if="isAuthenticated">fotos</router-link>
-                    </li>
-                    <li>
-                        <router-link class="nav-link" to="/footer">footer</router-link>
-                    </li>
-                <button @click="logout">salir</button>    
-                </ul>
-            </div>
+          <router-link class="navbar-brand" to="/">app</router-link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">menu</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/registroDeUsuario">registrarse</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/login">login</router-link>
+              </li>
+              <li class="nav-item" v-if="isAuthenticated()">
+                <router-link class="nav-link" to="/fotos">fotos</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/footer">footer</router-link>
+              </li>
+              <li class="nav-item">
+                <button @click="logout" class="nav-link btn btn-link">salir</button>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
-</template>
-
+      </nav>
+    </div>
+  </template>
+  
 <script>
-import {useRouter} from "vue-router"
+import { useRouter } from 'vue-router';
 
-const logout = () => {
-    localStorage.removeItem("usuario")
-    router.push("/login")
-}
-const isAuthenticated = () => {
-    return localStorage.getItem("usuario") != null
-}
-
+export default {
+  data() {
+    return {
+      msg: 'Bienvenido a tu aplicación',
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('usuario');
+      this.$router.push('/login');
+    },
+    isAuthenticated() {
+      return localStorage.getItem('usuario') !== null;
+    },
+  },
+};
 </script>
